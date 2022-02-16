3f32fd230e6c6431b2efcf60f1708a292b0702af
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-16 11:23:45 +0200

    Change path

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/register', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Register Page');
 });
 
