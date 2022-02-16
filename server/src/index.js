05316d618a5c830ea31b2ac68dcd2cb43d47f6d2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-16 14:34:09 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
-//const router = express.Router();
+
 const routes = require('./routes/routes');
 const mongooseConfige = require('./config/configMongoose');
 
 const port = 5000;
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('hello world')
-//   })
 
+app.use(cors());
 app.use(routes);
 mongooseConfige(app);
 
