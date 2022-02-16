509acad627995b0e89fa1fc2fb05d9c0e25d117e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-16 12:13:18 +0200

    Require mongoose config

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 //const router = express.Router();
 const routes = require('./routes/routes');
+const mongooseConfige = require('./config/configMongoose');
 
 const port = 5000;
 //app.use(express.json());
@@ -11,5 +12,6 @@ const port = 5000;
 //   })
 
 app.use(routes);
+mongooseConfige(app);
 
 app.listen(port, () => console.log("Server listenen on port " + `${port}`));
