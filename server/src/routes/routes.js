a3b8905b0193f954a6e4548552495b865db26356
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-16 11:23:17 +0200

    Added route

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const menuController = require('../controllers/menuController');
 
 router.use('/user', userController);
+router.use('/menu', menuController);
 
 module.exports = router;
