e83b3e1f6145b67cae07d244ba7d98a2980d9a84
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 10:25:18 +0200

    Create PageOne.js

---
+++ client/src/components/PageOne/PageOne.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header/Header';
+import SectionPageOne from '../Sections/SectionPageOne';
+
+const PageOne = () => {
+  return (
+    <div id="page1">
+        <div className="bg">
+            <Header />
+            <SectionPageOne />
+        </div>
+        
+    </div>
+  )
+}
+
+export default PageOne
