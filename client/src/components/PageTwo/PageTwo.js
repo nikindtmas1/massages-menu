e5dfd954f508e3062626c727832c14a176cbbedd
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 12:20:36 +0200

    Create PageTwo

---
+++ client/src/components/PageTwo/PageTwo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import SectionPageTwo from '../Sections/SectionPageTwo';
+
+const PageTwo = () => {
+  return (
+    <div id="page2">
+        <div className='bg'>
+            <Header />
+            <SectionPageTwo />
+        </div>
+    </div>
+  )
+}
+
+export default PageTwo
