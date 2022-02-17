c63aebea99dc1bf88fcdcd5f09ab46adbf94200e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 12:20:25 +0200

    Create ArticlePageTwo

---
+++ client/src/components/Articles/ArticlesPageTwo.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ArticlesPageTwo = () => {
+  return (
+    <>
+          <article class="grid_9">
+            <h3 class="p2">Operations Consulting</h3>
+            <div class="wrapper indent-bot">
+              <figure class="img-indent border"><img src="images/page2-img1.jpg" alt="" /></figure>
+              <div class="extra-wrap">
+                <h4>Sed ut perspiciatis unde omnis </h4>
+                Consectetur adipcing elsed eiusmod tempor incidint utbore dolore magna aliquat enim ad minim veniam, quis nostrud exercitation ullamco laborum et dolorum laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+            </div>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta. </article>
+          <article class="grid_3">
+            <h3 class="p2">Services List</h3>
+            <ul class="list-1">
+              <li><a href="/">Nailcare</a></li>
+              <li><a href="/">Haircare</a></li>
+              <li><a href="/">Face Lifting</a></li>
+              <li><a href="/">Spa Massage</a></li>
+              <li><a href="/">Skin Treatments</a></li>
+              <li><a href="/">Body Treatments</a></li>
+              <li><a href="/">Waxing</a></li>
+              <li><a href="/">Makeup</a></li>
+              <li><a href="/">Massage Therapy</a></li>
+            </ul>
+          </article>
+    </>
+  )
+}
+
+export default ArticlesPageTwo
