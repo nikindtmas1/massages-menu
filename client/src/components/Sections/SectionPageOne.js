3931432cd9900d80c0121ff424419a5816377ddd
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 10:25:31 +0200

    Create SectionPageOne.js

---
+++ client/src/components/Sections/SectionPageOne.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ArticlesPageOne from "../Articles/ArticlesPageOne";
+
+const SectionPageOne = () => {
+  return (
+    <section id="content">
+      <div className="main">
+        <div className="container_12">
+          <div className="wrapper">
+            <ArticlesPageOne />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionPageOne;
+
