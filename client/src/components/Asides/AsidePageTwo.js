f4c803c2518f92982f0746e2924fc06febfcb0f7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 13:07:13 +0200

    Create AsidePageTwo

---
+++ client/src/components/Asides/AsidePageTwo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AsideArticleTwo from './AsideArticles/AsideArticleTwo';
+
+const AsidePageTwo = () => {
+  return (
+    <aside>
+  <div className="main">
+    <div className="container_12">
+      <div className="wrapper">
+        <AsideArticleTwo />
+      </div>
+    </div>
+  </div>
+</aside>
+  )
+}
+
+export default AsidePageTwo
