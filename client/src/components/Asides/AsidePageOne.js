1a4df6a64a3c2a77e29dfaaf28112ecc7605f935
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 11:40:49 +0200

    Create AsidePageOne

---
+++ client/src/components/Asides/AsidePageOne.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AsideArticleOne from './AsideArticles/AsideArticleOne';
+
+const AsidePageOne = () => {
+  return (
+    <aside>
+    <div className="main">
+      <div className="container_12">
+        <div className="wrapper">
+            <AsideArticleOne />
+        </div>
+      </div>
+    </div>
+  </aside>
+  )
+}
+
+export default AsidePageOne
