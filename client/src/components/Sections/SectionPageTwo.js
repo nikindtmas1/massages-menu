d85633197cbe292c9d43a9719c5cf9f989023b9b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-17 12:20:50 +0200

    Create SectionPageTwo

---
+++ client/src/components/Sections/SectionPageTwo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticlesPageTwo from '../Articles/ArticlesPageTwo';
+
+const SectionPageTwo = () => {
+  return (
+    <section id="content">
+    <div className="main">
+      <div className="container_12">
+        <div className="wrapper">
+         <ArticlesPageTwo />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionPageTwo
