75279bbebe73d3848cfe22dd1b4304b8c7a7e80f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 15:19:14 +0200

    Create ArticlePageMassages

---
+++ client/src/components/Articles/ArticlesPageMassages.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const ArticlesPageMassages = () => {
+  return (
+    <article className="grid_12">
+    <h3 className="p2">Therapies</h3>
+    <div className="wrapper p4">
+      <div className="grid_4 alpha">
+        <div className="wrapper p2">
+          <figure className="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
+        </div>
+        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        <Link className="button" to="#">Details</Link> </div>
+      <div className="grid_4">
+        <div className="indent-left3">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
+          </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
+      </div>
+      <div className="grid_4 omega">
+        <div className="indent-left2">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
+          </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
+      </div>
+    </div>
+    <div className="wrapper">
+      <div className="grid_4 alpha">
+        <div className="wrapper p2">
+          <figure className="border fleft"><img src="images/page3-img4.jpg" alt="" /></figure>
+        </div>
+        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        <Link className="button" to="#">Details</Link> </div>
+      <div className="grid_4">
+        <div className="indent-left3">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img5.jpg" alt="" /></figure>
+          </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
+      </div>
+      <div className="grid_4 omega">
+        <div className="indent-left2">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img6.jpg" alt="" /></figure>
+          </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
+      </div>
+    </div>
+  </article>
+  )
+}
+
+export default ArticlesPageMassages
