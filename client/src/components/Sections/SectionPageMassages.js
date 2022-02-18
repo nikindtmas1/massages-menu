2316aa6b66ddf60f4b547bc603ffe9351c5be7a6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 15:20:08 +0200

    Create SectionPageMassages

---
+++ client/src/components/Sections/SectionPageMassages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ArticlesPageMassages from '../Articles/ArticlesPageMassages';
+
+const SectionPageMassages = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+            <ArticlesPageMassages />
+         {/* <ArticlesPageThree /> */}
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionPageMassages
