584dc9c8d1daf3c8a3772c68c87e7356db21bec6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 13:57:00 +0200

    Create SectionPageFive

---
+++ client/src/components/Sections/SectionPageFive.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticlesPageFive from '../Articles/ArticlesPageFive';
+
+const SectionPageFive = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+         <ArticlesPageFive />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionPageFive
