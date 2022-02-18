85a06a7aa14a82543d23b20a03ea61b630009e19
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 12:47:23 +0200

    Update ArticlesPageTwo

--- client/src/components/Articles/ArticlesPageTwo.js
+++ client/src/components/Articles/ArticlesPageTwo.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const ArticlesPageTwo = () => {
   return (
     <>
-          <article class="grid_9">
-            <h3 class="p2">Operations Consulting</h3>
-            <div class="wrapper indent-bot">
-              <figure class="img-indent border"><img src="images/page2-img1.jpg" alt="" /></figure>
-              <div class="extra-wrap">
+          <article className="grid_9">
+            <h3 className="p2">Operations Consulting</h3>
+            <div className="wrapper indent-bot">
+              <figure className="img-indent border"><img src="images/page2-img1.jpg" alt="" /></figure>
+              <div className="extra-wrap">
                 <h4>Sed ut perspiciatis unde omnis </h4>
                 Consectetur adipcing elsed eiusmod tempor incidint utbore dolore magna aliquat enim ad minim veniam, quis nostrud exercitation ullamco laborum et dolorum laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
             </div>
             At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta. </article>
-          <article class="grid_3">
-            <h3 class="p2">Services List</h3>
-            <ul class="list-1">
-              <li><a href="/">Nailcare</a></li>
-              <li><a href="/">Haircare</a></li>
-              <li><a href="/">Face Lifting</a></li>
-              <li><a href="/">Spa Massage</a></li>
-              <li><a href="/">Skin Treatments</a></li>
-              <li><a href="/">Body Treatments</a></li>
-              <li><a href="/">Waxing</a></li>
-              <li><a href="/">Makeup</a></li>
-              <li><a href="/">Massage Therapy</a></li>
+          <article className="grid_3">
+            <h3 className="p2">Services List</h3>
+            <ul className="list-1">
+              <li><Link to="/">Nailcare</Link></li>
+              <li><Link to="/">Haircare</Link></li>
+              <li><Link to="/">Face Lifting</Link></li>
+              <li><Link to="/">Spa Massage</Link></li>
+              <li><Link to="/">Skin Treatments</Link></li>
+              <li><Link to="/">Body Treatments</Link></li>
+              <li><Link to="/">Waxing</Link></li>
+              <li><Link to="/">Makeup</Link></li>
+              <li><Link to="/">Massage Therapy</Link></li>
             </ul>
           </article>
     </>
