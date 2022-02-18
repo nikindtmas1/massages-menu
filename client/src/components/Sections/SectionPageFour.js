cc2e58bbb008104b893fc3ce582b6f38d0d7f07f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 12:48:07 +0200

    Create SectionPageFour

---
+++ client/src/components/Sections/SectionPageFour.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticlesPageFour from '../Articles/ArticlesPageFour';
+
+const SectionPageFour = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper p4">
+        <ArticlesPageFour />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionPageFour
