2570d103b04827cc7d0468795fff0ef36e989053
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 15:19:43 +0200

    Create BodyMassages

---
+++ client/src/components/BodyMassages/BodyMassages.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import SectionPageMassages from "../Sections/SectionPageMassages";
+
+const BodyMassages = () => {
+  return (
+    <div id="page5">
+      <div className="bg">
+          <Header />
+          <SectionPageMassages />
+          </div>
+        <Footer />
+    </div>
+  );
+};
+
+export default BodyMassages;
