08a69d68257647edd3d01fd56f11341dc705f722
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 13:56:36 +0200

    Create AsidePageFive

---
+++ client/src/components/Asides/AsidePageFive.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AsideArticlesFive from './AsideArticles/AsideArticlesFive';
+
+const AsidePageFive = () => {
+  return (
+    <aside>
+  <div class="main">
+    <div class="container_12">
+      <div class="wrapper">
+        <AsideArticlesFive />
+      </div>
+    </div>
+  </div>
+</aside>
+  )
+}
+
+export default AsidePageFive
