4ad5cf3133b61a1b22f3d0c5112553b184cbad1d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 12:10:29 +0200

    Create SectionPageThree

---
+++ client/src/components/Sections/SectionPageThree.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const SectionPageThree = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+          <article class="grid_12">
+            <h3 class="p2">Therapies</h3>
+            <div class="wrapper p4">
+              <div class="grid_4 alpha">
+                <div class="wrapper p2">
+                  <figure class="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
+                </div>
+                <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                <a class="button" href="#">Details</a> </div>
+              <div class="grid_4">
+                <div class="indent-left3">
+                  <div class="wrapper p2">
+                    <figure class="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
+                  </div>
+                  <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                  <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                  <a class="button" href="#">Details</a> </div>
+              </div>
+              <div class="grid_4 omega">
+                <div class="indent-left2">
+                  <div class="wrapper p2">
+                    <figure class="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
+                  </div>
+                  <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                  <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                  <a class="button" href="#">Details</a> </div>
+              </div>
+            </div>
+            <div class="wrapper">
+              <div class="grid_4 alpha">
+                <div class="wrapper p2">
+                  <figure class="border fleft"><img src="images/page3-img4.jpg" alt="" /></figure>
+                </div>
+                <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                <a class="button" href="#">Details</a> </div>
+              <div class="grid_4">
+                <div class="indent-left3">
+                  <div class="wrapper p2">
+                    <figure class="border fleft"><img src="images/page3-img5.jpg" alt="" /></figure>
+                  </div>
+                  <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                  <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                  <a class="button" href="#">Details</a> </div>
+              </div>
+              <div class="grid_4 omega">
+                <div class="indent-left2">
+                  <div class="wrapper p2">
+                    <figure class="border fleft"><img src="images/page3-img6.jpg" alt="" /></figure>
+                  </div>
+                  <h6 class="prev-indent-bot"><a class="link" href="#">Lorem ipsum dolor sit amet</a></h6>
+                  <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                  <a class="button" href="#">Details</a> </div>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionPageThree
