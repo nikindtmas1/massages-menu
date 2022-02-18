c6191b3e4457d07820b8ed4614653b9b36d4fde1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-18 12:47:02 +0200

    Update ArticlesPageThree

--- client/src/components/Articles/ArticlesPageThree.js
+++ client/src/components/Articles/ArticlesPageThree.js
@@ -3,60 +3,60 @@ import {Link} from 'react-router-dom';
 
 const ArticlesPageThree = () => {
   return (
-    <article class="grid_12">
-    <h3 class="p2">Therapies</h3>
-    <div class="wrapper p4">
-      <div class="grid_4 alpha">
-        <div class="wrapper p2">
-          <figure class="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
+    <article className="grid_12">
+    <h3 className="p2">Therapies</h3>
+    <div className="wrapper p4">
+      <div className="grid_4 alpha">
+        <div className="wrapper p2">
+          <figure className="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
         </div>
-        <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-        <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-        <Link class="button" to="#">Details</Link> </div>
-      <div class="grid_4">
-        <div class="indent-left3">
-          <div class="wrapper p2">
-            <figure class="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
+        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        <Link className="button" to="#">Details</Link> </div>
+      <div className="grid_4">
+        <div className="indent-left3">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
           </div>
-          <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link class="button" to="#">Details</Link> </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
       </div>
-      <div class="grid_4 omega">
-        <div class="indent-left2">
-          <div class="wrapper p2">
-            <figure class="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
+      <div className="grid_4 omega">
+        <div className="indent-left2">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
           </div>
-          <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link class="button" to="#">Details</Link> </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
       </div>
     </div>
-    <div class="wrapper">
-      <div class="grid_4 alpha">
-        <div class="wrapper p2">
-          <figure class="border fleft"><img src="images/page3-img4.jpg" alt="" /></figure>
+    <div className="wrapper">
+      <div className="grid_4 alpha">
+        <div className="wrapper p2">
+          <figure className="border fleft"><img src="images/page3-img4.jpg" alt="" /></figure>
         </div>
-        <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-        <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-        <Link class="button" to="#">Details</Link> </div>
-      <div class="grid_4">
-        <div class="indent-left3">
-          <div class="wrapper p2">
-            <figure class="border fleft"><img src="images/page3-img5.jpg" alt="" /></figure>
+        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        <Link className="button" to="#">Details</Link> </div>
+      <div className="grid_4">
+        <div className="indent-left3">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img5.jpg" alt="" /></figure>
           </div>
-          <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link class="button" to="#">Details</Link> </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
       </div>
-      <div class="grid_4 omega">
-        <div class="indent-left2">
-          <div class="wrapper p2">
-            <figure class="border fleft"><img src="images/page3-img6.jpg" alt="" /></figure>
+      <div className="grid_4 omega">
+        <div className="indent-left2">
+          <div className="wrapper p2">
+            <figure className="border fleft"><img src="images/page3-img6.jpg" alt="" /></figure>
           </div>
-          <h6 class="prev-indent-bot"><Link class="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p class="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link class="button" to="#">Details</Link> </div>
+          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          <Link className="button" to="#">Details</Link> </div>
       </div>
     </div>
   </article>
