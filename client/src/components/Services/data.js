79716f9b701c67561cb6f2e8074d965a4a0a230e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-19 17:20:24 +0200

    Create data

---
+++ client/src/components/Services/data.js
@@ -0,0 +1,10 @@
+import * as service from '../Services/massagesService';
+
+const host = 'http://localhost:5000';
+service.settings.host = host;
+
+export async function getAll(){
+    let massages = await service.get(host + '/massages');
+    let result = Object.values(massages);
+    return result;
+};
