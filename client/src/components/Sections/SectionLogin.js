5f55f7d27b13b1a1b8d6b86ab3b1e6e8f7d2a146
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-20 16:25:20 +0200

    Create SectionLogin

---
+++ client/src/components/Sections/SectionLogin.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticleLogin from '../Articles/ArticleLogin';
+
+const SectionLogin = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+       <ArticleLogin />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionLogin
