509c10b6eeea4c0cfb7453698a83858eac188cb0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-20 16:14:40 +0200

    update register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import * as userService from '../Services/data';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import SectionRegister  from '../Sections/SectionRegister';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    const history = useHistory();
+    return (
+        <div id='page3'>
+        <div className='bg'>
+            <Header />
+            <SectionRegister />
+        </div>
+        <Footer />
+        </div>
+    )
 }
 
 export default Register
