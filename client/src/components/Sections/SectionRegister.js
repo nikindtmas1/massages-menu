bd97de9cd7dfdaae60f7f188084b61ab12b3f93b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-20 16:14:53 +0200

    Create SectionRegister

---
+++ client/src/components/Sections/SectionRegister.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticlesRegister from '../Articles/ArticlesRegister';
+
+const SectionRegister = () => {
+  return (
+    <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+        <ArticlesRegister />
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionRegister
