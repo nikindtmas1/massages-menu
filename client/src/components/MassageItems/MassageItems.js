632ed0a378e3902e2e45724e9a813b5af721c380
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-21 14:43:40 +0200

    Update MassageItems

--- client/src/components/MassageItems/MassageItems.js
+++ client/src/components/MassageItems/MassageItems.js
@@ -14,11 +14,12 @@ const MassageItems = ({massages}) => {
             <figure className="border fleft"><img src={x.img} alt="" /></figure>
           </div>
           <h6 className="prev-indent-bot"><Link className="link" to="#">{x.name}</Link></h6>
-          <p className="indent-bot">{x.description}</p>
+          {/* <p className="indent-bot">{x.description}</p> */}
           <p className='indent-bot'>
-              <span>time: {x.time}</span>
+              <span style={{'padding':"10px","color":"blue"}}>{x.time} min</span>
               <span>       </span>
-              <span>price: {x.price}</span>
+         
+              <span style={{'padding-left':'120px', 'color':"blue"}}>{x.price} BGN</span>
               </p>
           {/* <Link className="button" to="#">Details</Link>  */}
           </div>
