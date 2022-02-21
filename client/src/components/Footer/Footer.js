682a3a8ce74c3423a668891d1478c2cbf75f4dd1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-21 15:16:27 +0200

    Update footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,15 +9,14 @@ const Footer = () => {
               <div className="grid_8">
                 <div className="footer-text">
                   <b>
-                    Copyright &copy; <a href="/">Domain Name</a> All Rights
-                    Reserved
+                    Copyright &copy; <a href="/">Created by</a> Nikolay Nikolaev
                   </b>{" "}
                   Design by{" "}
                   <a  href="http://www.templatemonster.com/">
                     TemplateMonster.com
                   </a>
                 </div>
-                <ul className="list-services">
+                {/* <ul className="list-services">
                   <li>
                     <a className="item-1" href="/">'</a>
                   </li>
@@ -30,14 +29,14 @@ const Footer = () => {
                   <li>
                     <a className="item-4" href="/">'</a>
                   </li>
-                </ul>
+                </ul> */}
               </div>
-              <div className="grid_4">
+              {/* <div className="grid_4">
                 <div className="support-phone">
                   {" "}
-                  <strong>Toll Free:</strong> <b>8(800)</b>154-45-67{" "}
+                  <strong>Call:</strong> <b>+(359)</b>88-219-6877{" "}
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
