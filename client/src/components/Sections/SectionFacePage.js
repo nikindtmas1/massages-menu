08dd1da828bc19a1474765f733acb50377b43939
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 09:58:06 +0200

    Create SectionFacePage

---
+++ client/src/components/Sections/SectionFacePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArticlesFacePage from '../Articles/ArticlesFacePage';
+
+const SectionFacePage = () => {
+  return (
+    <section >
+        <ArticlesFacePage />
+    </section>
+  )
+}
+
+export default SectionFacePage
