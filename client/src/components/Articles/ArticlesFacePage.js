2bd0454546a400edf6a270eebab3fd846f8587f1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 11:33:35 +0200

    Update ArticlesFacePage

--- client/src/components/Articles/ArticlesFacePage.js
+++ client/src/components/Articles/ArticlesFacePage.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import FaceItems from '../FaceItems/FaceItems';
 
 const ArticlesFacePage = () => {
+
+    const [faceCare, setFaceCare] = useState([]);
+
+    useEffect(() => {
+        
+    },[]);
+
   return (
     <article className="grid_12">
     <h3 className="p2">Face Care</h3>
     <div className="wrapper p4">
 
-    <FaceItems />
+    <FaceItems faceCare={faceCare} />
     </div>
   </article>
   )
