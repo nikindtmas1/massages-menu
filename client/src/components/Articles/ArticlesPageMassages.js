23d2a30de854b052842d36a0d3830c8a7ec12684
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 09:56:22 +0200

    Remove link

--- client/src/components/Articles/ArticlesPageMassages.js
+++ client/src/components/Articles/ArticlesPageMassages.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
 
 import * as services from '../Services/data';
 
@@ -17,33 +16,9 @@ useEffect(() => {
     <article className="grid_12">
     <h3 className="p2">Body Massages</h3>
     <div className="wrapper p4">
-      {/* <div className="grid_4 alpha">
-        <div className="wrapper p2">
-          <figure className="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
-        </div>
-        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-        <Link className="button" to="#">Details</Link> </div>
-      <div className="grid_4">
-        <div className="indent-left3">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div>
-      <div className="grid_4 omega">
-        <div className="indent-left2">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div> */}
-    </div>
+
     <MassageItems massages={massages}/>
+    </div>
   </article>
   )
 }
