f83e3eea07ee6a9680ca6be43d19d0730cfcf68c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 11:11:11 +0200

    Create FaceItem

---
+++ client/src/components/FaceItems/FaceItems.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FaceItems = () => {
+  return (
+    <div className='wrapper'>
+    {/* map */}
+    <div className="listing">
+    <div className="preview">
+        <img src='images/page3-img5.jpg' />
+    </div>
+    {/* <h5>type: name:</h5> */}
+    <div className="info">
+        <div className="data-info" >
+            <h5 >Time: 50 minute</h5>
+            <h5>Price: 180 BGN</h5>
+        </div>
+       
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default FaceItems
