a4b35d614fb09c9442544bc1c28ca2f7a57354f1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 09:57:49 +0200

    Create ArticleFacePage

---
+++ client/src/components/Articles/ArticlesFacePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ArticlesFacePage = () => {
+  return (
+    <article className="grid_12">
+    <h3 className="p2">Face Care</h3>
+    <div className="wrapper p4">
+
+    
+    </div>
+  </article>
+  )
+}
+
+export default ArticlesFacePage
