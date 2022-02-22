86430e70dbe550a14dbb1de4bb1648d7c4476b8d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 11:51:11 +0200

    Update routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 const massagesController = require('../controllers/massagesController');
+const facesController = require('../controllers/facesController');
 
 router.use('/users', userController);
 router.use('/massages', massagesController);
+router.use('/faces', facesController);
 
 module.exports = router;
