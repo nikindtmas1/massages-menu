dc48f66cf55218f8fb7dd0a5fa3be1f1cae7f052
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 11:10:45 +0200

    Import FaceItem

--- client/src/components/Articles/ArticlesFacePage.js
+++ client/src/components/Articles/ArticlesFacePage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import FaceItems from '../FaceItems/FaceItems';
 
 const ArticlesFacePage = () => {
   return (
@@ -6,7 +7,7 @@ const ArticlesFacePage = () => {
     <h3 className="p2">Face Care</h3>
     <div className="wrapper p4">
 
-    
+    <FaceItems />
     </div>
   </article>
   )
