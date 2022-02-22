e00c309f86e1a06c507885145ce076cbfdb0f39f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 11:11:40 +0200

    Update SectionFacePage

--- client/src/components/Sections/SectionFacePage.js
+++ client/src/components/Sections/SectionFacePage.js
@@ -4,7 +4,14 @@ import ArticlesFacePage from '../Articles/ArticlesFacePage';
 const SectionFacePage = () => {
   return (
     <section >
+        <div className="main">
+      <div className="container_12">
+        <div className="wrapper">
         <ArticlesFacePage />
+        </div>
+      </div>
+    </div>
+       
     </section>
   )
 }
