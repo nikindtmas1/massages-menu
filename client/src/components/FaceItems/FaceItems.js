a2c13de0e88ca84a607d1dd621a782bc456e3dd8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 12:24:14 +0200

    Update FacePage

--- client/src/components/FaceItems/FaceItems.js
+++ client/src/components/FaceItems/FaceItems.js
@@ -4,20 +4,24 @@ import {Link} from 'react-router-dom';
 const FaceItems = ({faceCare}) => {
   return (
     <div className='wrapper'>
-    {/* map */}
-    <div className="listing">
-    <div className="preview">
-        <img src='images/page3-img5.jpg' />
-    </div>
-    <Link to='/'><h5>name: Lifting</h5></Link>
-    <div className="info">
-        <div className="data-info" >
-            <h5 >Time: 50 minute</h5>
-            <h6>Price: 180 BGN</h6>
-        </div>
-       
-    </div>
-    </div>
+    {faceCare.map(x => 
+    <div className='grid_4 alpha'>
+         <div className="listing">
+         <div className="preview">
+             <img src={x.img} />
+         </div>
+         <Link to='/'><h5>{x.name}</h5></Link>
+         <div className="info">
+             <div className="data-info" >
+                 <h5 >{x.time} min</h5>
+                 <h6>{x.price} BGN</h6>
+             </div>
+            
+         </div>
+         </div>
+         </div>
+        )}
+   
     </div>
   )
 }
