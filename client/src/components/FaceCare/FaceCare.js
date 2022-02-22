850b36e1faffacf71b499251efe49f2a7edc5a6e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 09:56:49 +0200

    Added Section FacePage

--- client/src/components/FaceCare/FaceCare.js
+++ client/src/components/FaceCare/FaceCare.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
+import SectionFacePage from '../Sections/SectionFacePage';
 
 const FaceCare = () => {
   return (
     <div id='page3'>
         <div className='bg'>
             <Header />
+            <SectionFacePage />
         </div>
     </div>
   )
