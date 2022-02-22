be78dff5022e1206efea9d7fac53f79650ae6d57
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-22 16:17:56 +0200

    Update LoginPage

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -3,7 +3,11 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SectionLogin from '../Sections/SectionLogin';
 
+
+
 const LoginPage = () => {
+
+  
   return (
     <div id='page3'>
         <div className='bg'>
