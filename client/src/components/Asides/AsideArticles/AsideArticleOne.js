d2698ba473106b5bf49236ad7e742de248a79aaa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-23 15:47:07 +0200

    Added href

--- client/src/components/Asides/AsideArticles/AsideArticleOne.js
+++ client/src/components/Asides/AsideArticles/AsideArticleOne.js
@@ -33,31 +33,14 @@ const AsideArticleOne = () => {
         <div className="indent-left2">
           <h3 className="p1">Spa Rest</h3>
           <p className="indent-bot">
-            This website template has several pages:{" "}
-            <Link className="link" to="index.html">
-              About Us
-            </Link>
-            ,{" "}
-            <Link className="link" to="services.html">
-              Our Services
-            </Link>
-            ,{" "}
-            <Link className="link" to="therapies.html">
-              Therapies
-            </Link>
-            ,{" "}
-            <Link className="link" to="staff.html">
-              Our Staff
-            </Link>
-            ,{" "}
-            <Link className="link" to="contacts.html">
-              Contact Us
-            </Link>{" "}
-            (please note that contact us form doesn’t work).
+          This SpaSalon Template goes with two pack ages – with PSD source
+            files (are available for free for the registered members of
+            Templates.com) and without them.
+         
           </p>
-          <Link className="button" to="/">
+          <a className="button" href="https://www.hotelcasinointernational.com/offer/hot-summer-offer">
             Read More
-          </Link>{" "}
+          </a>{" "}
         </div>
       </article>
     </>
