9679faa64b6e8a79e4b1ca4d179259cec0d0f4b1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-23 14:49:59 +0200

    Update ArticlePageWaxing

--- client/src/components/Articles/ArticlesPageWaxing.js
+++ client/src/components/Articles/ArticlesPageWaxing.js
@@ -1,66 +1,64 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{useState, useEffect} from 'react';
+//import {Link} from 'react-router-dom';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core'; 
+
+import * as services from '../Services/data';
+
+import useStyles from './stylesWaxing';
 
 const ArticlesPageWaxing = () => {
+    const classes = useStyles();
+    const [waxTherapies, setWaxTherapies] = useState([]);
+
+    useEffect(() => {
+        services.getAll()
+        .then((result) => setWaxTherapies(result))
+    },[]);
+
   return (
-    <article className="grid_12">
-    <h3 className="p2">Therapies</h3>
-    <div className="wrapper p4">
-      <div className="grid_4 alpha">
-        <div className="wrapper p2">
-          <figure className="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
-        </div>
-        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-        <Link className="button" to="#">Details</Link> </div>
-      <div className="grid_4">
-        <div className="indent-left3">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img2.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div>
-      <div className="grid_4 omega">
-        <div className="indent-left2">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img3.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div>
-    </div>
-    <div className="wrapper">
-      <div className="grid_4 alpha">
-        <div className="wrapper p2">
-          <figure className="border fleft"><img src="images/page3-img4.jpg" alt="" /></figure>
-        </div>
-        <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-        <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-        <Link className="button" to="#">Details</Link> </div>
-      <div className="grid_4">
-        <div className="indent-left3">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img5.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div>
-      <div className="grid_4 omega">
-        <div className="indent-left2">
-          <div className="wrapper p2">
-            <figure className="border fleft"><img src="images/page3-img6.jpg" alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
-          <p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <Link className="button" to="#">Details</Link> </div>
-      </div>
-    </div>
-  </article>
+      <>
+    {/* <article className="grid_12"> */}
+    <h3 className="p2">Waxing Therapies</h3>
+    {/* <div className="wrapper p4"> */}
+    {waxTherapies.map(x => 
+         <Card className={classes.root}>
+         <CardMedia className={classes.media} image={x.img} title={x.name} />
+         <CardContent>
+             <div className={classes.cardContent}>
+                 <Typography variant='h5' gutterBottom>
+                     {x.name}
+                 </Typography>
+                 <Typography variant='h6'>
+                    {x.time} min    {x.price} BGN
+                 </Typography>
+             </div>
+             {/* <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant='body2' color='textSecondary' /> */}
+         </CardContent>
+         {/* <CardActions disableSpacing className={classes.cardActions} >
+             <IconButton aria-label='Add to Cart' onClick={() => onAddToCart(product.id, 1)}>
+                 <AddShoppingCart  />
+             </IconButton>
+         </CardActions> */}
+         <br />
+         <br />
+     </Card>
+        
+        )}
+  
+    {/* </div> */}
+
+    {/* </article> */}
+</>
   )
 }
 
-export default ArticlesPageWaxing
+export default ArticlesPageWaxing;
+
+{/* <div className="grid_4 alpha">
+<div className="wrapper p2">
+  <figure className="border fleft"><img src="images/page3-img1.jpg" alt="" /></figure>
+</div>
+<h6 className="prev-indent-bot"><Link className="link" to="#">Lorem ipsum dolor sit amet</Link></h6>
+<p className="indent-bot">Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+<Link className="button" to="#">Details</Link>
+</div> */}
