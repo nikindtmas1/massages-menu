063b7bb30d30039443a8b263f5c7717d0435993f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-23 13:50:23 +0200

    Create SectionWaxingPage

---
+++ client/src/components/Sections/SectionWaxingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ArticlesPageWaxing from '../Articles/ArticlesPageWaxing';
+
+const SectionWaxingPage = () => {
+  return (
+    <section id="content">
+    <div className="main">
+      <div className="container_12">
+        <div className="wrapper">
+         <ArticlesPageWaxing />
+
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default SectionWaxingPage
