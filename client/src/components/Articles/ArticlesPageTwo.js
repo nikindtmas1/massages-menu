b7ded613bfcf4958df1a263dc7f296dbbe8df691
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-23 16:48:17 +0200

    Added images ang text

--- client/src/components/Articles/ArticlesPageTwo.js
+++ client/src/components/Articles/ArticlesPageTwo.js
@@ -9,10 +9,13 @@ const ArticlesPageTwo = () => {
             <div className="wrapper indent-bot">
               <figure className="img-indent border"><img src="images/page2-img1.jpg" alt="" /></figure>
               <div className="extra-wrap">
-                <h4>Sed ut perspiciatis unde omnis </h4>
-                Consectetur adipcing elsed eiusmod tempor incidint utbore dolore magna aliquat enim ad minim veniam, quis nostrud exercitation ullamco laborum et dolorum laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+                <h4>The “I-SPA” Center in Golden Sands </h4>
+                Walk into a dream oasis of over 100 specially curated spa treatments and exotic beauty products, designed to revive and unwind your mind, body and soul. You deserve it.
+The “I-SPA” Center in Golden Sands /Varna, Bulgaria/ offers additional facilities including, indoor pool, steam bath, Turkish bath, infrared sauna, Finnish sauna, hydro-massage room, fitness room and a beauty salon.
+Due to the COVID-19 situation, some of the SPA procedures might be temporary not available. . </div>
             </div>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta. </article>
+            To complete your wellness package, we invite you to enjoy our indoor swimming pool or top up your relaxation experience in the Hydro-massage bathtub.
+            </article>
           <article className="grid_3">
             <h3 className="p2">Services List</h3>
             <ul className="list-1">
--- client/src/components/Asides/AsideArticles/AsideArticleTwo.js
+++ client/src/components/Asides/AsideArticles/AsideArticleTwo.js
@@ -12,22 +12,24 @@ const AsideArticleTwo = () => {
           <p className="indent-bot">Sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
           <h6 className="prev-indent-bot"><Link className="link" to="/">Eaque ipsa quae ab illo inventore veritatis et quasi </Link></h6>
           <p className="indent-bot">Architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptaporro quisquam est, qui dolorem ipsum quia dolor.</p>
-          <Link className="button" to="/">Read More</Link> </article>
+          {/* <Link className="button" to="/">Read More</Link>  */}
+          </article>
         <article className="grid_6">
           <div className="indent-left2">
             <h3 className="prev-indent-bot2">Featured Services</h3>
             <div className="wrapper p3">
-              <figure className="img-indent border"><Link to="/"><img src="images/page2-img2.jpg" alt="" /></Link></figure>
+              <figure className="img-indent border"><a href="https://www.hotelcasinointernational.com/page/spapools/indoor-pool-lounge"><img src="images/page2-img2.jpg" alt="" /></a></figure>
               <div className="extra-wrap">
-                <h6 className="prev-indent-bot"><Link className="link" to="/">At vero eos et accusamus iusto</Link></h6>
-                Odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque upti sint occaecati cupiditate non provident, mollitia animi, id est laborum et dolorum fugat harum quidem rerum. </div>
+                <h6 className="prev-indent-bot"><a className="link" href="https://www.hotelcasinointernational.com/page/spapools/indoor-pool-lounge">Indoor Pool & Lounge</a></h6>
+                To complete your wellness package, we invite you to enjoy our indoor swimming pool or top up your relaxation experience in the Hydro-massage bathtub. </div>
             </div>
             <div className="wrapper">
-              <figure className="img-indent border"><Link to="/"><img src="images/page2-img3.jpg" alt="" /></Link></figure>
+              <figure className="img-indent border"><a href="https://www.hotelcasinointernational.com/page/spapools/spa-heat-zone"><img src="images/page2-img3.jpg" alt="" /></a></figure>
               <div className="extra-wrap">
-                <h6 className="prev-indent-bot"><Link className="link" to="/">Nam libero tempore cum</Link></h6>
-                <p className="indent-bot">Soluta nobis est eligendi optio cumque nihil impedituo minus quod maxime placeat facere possimus, omnis voluptas assumautem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-                <Link className="button" to="/">Read More</Link> </div>
+                <h6 className="prev-indent-bot"><a className="link" href="https://www.hotelcasinointernational.com/page/spapools/spa-heat-zone">The Heat Zone</a></h6>
+                <p className="indent-bot">The alternating hot and cold cycles, as well as the periods of rest are characteristic of the classical sauna process and lead to improved blood circulation and a stronger immune system.</p>
+                {/* <Link className="button" to="/">Read More</Link>  */}
+                </div>
             </div>
           </div>
         </article>
