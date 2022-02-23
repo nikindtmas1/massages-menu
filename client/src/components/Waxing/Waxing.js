58d065fc4c95441381b69a34906684d71a180eee
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-23 12:49:09 +0200

    Create Waxing

---
+++ client/src/components/Waxing/Waxing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const Waxing = () => {
+  return (
+    <div id='page3'>
+        <div className='bg'>
+            <Header />
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default Waxing
