a3e0106627e0cbf8dd3c5dc1a3bd1aac8688613d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-24 15:08:57 +0200

    Update ArticlesPageWaxing

--- client/src/components/Articles/ArticlesPageWaxing.js
+++ client/src/components/Articles/ArticlesPageWaxing.js
@@ -1,13 +1,14 @@
 import React,{useState, useEffect} from 'react';
+import { Grid } from "@material-ui/core";
+
 //import {Link} from 'react-router-dom';
-import { Card, CardMedia, CardContent, Typography } from '@material-ui/core'; 
-
+import WaxingItems from '../WaxingItems/WaxingItems';
 import * as services from '../Services/data';
-
-import useStyles from './stylesWaxing';
+import useStyles from '../Articles/styleArticlesWax';
 
 const ArticlesPageWaxing = () => {
     const classes = useStyles();
+
     const [waxTherapies, setWaxTherapies] = useState([]);
 
     useEffect(() => {
@@ -20,31 +21,17 @@ const ArticlesPageWaxing = () => {
     <article className="grid_12">
     <h3 className="p2">Waxing Therapies</h3>
     <div className="wrapper p4">
-    {waxTherapies.map(x => 
-         <Card className={classes.root}>
-         <CardMedia className={classes.media} image={x.img} title={x.name} />
-         <CardContent>
-             <div className={classes.cardContent}>
-                 <Typography variant='h5' gutterBottom>
-                     {x.name}
-                 </Typography>
-                 <Typography variant='h6'>
-                    {x.time} min  {x.price} BGN
-                 </Typography>
-             </div>
-             {/* <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant='body2' color='textSecondary' /> */}
-         </CardContent>
-         {/* <CardActions disableSpacing className={classes.cardActions} >
-             <IconButton aria-label='Add to Cart' onClick={() => onAddToCart(product.id, 1)}>
-                 <AddShoppingCart  />
-             </IconButton>
-         </CardActions> */}
-         <br />
-         <br />
-     </Card>
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Grid container justifyContent='center' spacing={4}>
+            {waxTherapies.map((waxTherapy) => (
+                <Grid item key={waxTherapy.id} xs={12} sm={6} md={4} lg={3}>
+                    <WaxingItems waxTherapy={waxTherapy} />
+                </Grid>
+            ))}
+        </Grid>
+    </main>
         
-        )}
-  
     </div>
 
     </article>
