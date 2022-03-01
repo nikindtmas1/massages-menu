a16ddff4e2ac34a7d9db908823d390b7b2445e52
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-01 22:07:49 +0200

    Update waxingItems

--- client/src/components/WaxingItems/WaxingItems.js
+++ client/src/components/WaxingItems/WaxingItems.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
-import { Card, CardMedia, CardContent, Typography, IconButton, CardActions } from '@material-ui/core'; 
+import React, { useState } from 'react'
+import { Card, CardMedia, CardContent, Typography, IconButton, CardActions } from '@material-ui/core';
 import useStyles from '../Articles/stylesWaxing';
-import {ThumbUpAlt} from '@material-ui/icons';
+import { ThumbUpAlt } from '@material-ui/icons';
 
 
 
 
-const WaxingItems = ({waxTherapy}) => {
+const WaxingItems = ({ waxTherapy }) => {
 
     const classes = useStyles();
 
@@ -19,33 +19,39 @@ const WaxingItems = ({waxTherapy}) => {
 
 
 
-  return (
-      
+    return (
+
         <Card className={classes.root}>
-        <CardMedia className={classes.media} image={waxTherapy.img} title={waxTherapy.name} />
-        <CardContent>
-            <div className={classes.cardContent}>
-                <Typography variant='h5' gutterBottom>
-                    {waxTherapy.name}
-                </Typography>
-                <Typography variant='h6'>
-                   {waxTherapy.time} min {" "} -  {waxTherapy.price} BGN
-                </Typography>
-            </div>
-            {/* <Typography dangerouslySetInnerHTML={{ __html: waxTherapy.description }} variant='body2' color='textSecondary' /> */}
-        </CardContent>
-        <CardActions disableSpacing className={classes.cardActions} >
-            <IconButton aria-label='Add to Cart' onClick={onClick}>
-            
-                <ThumbUpAlt />
-                
-            </IconButton>
-        </CardActions>
-        <br />
-        <br />
+            <CardMedia className={classes.media} image={waxTherapy.img} title={waxTherapy.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {waxTherapy.name}
+                    </Typography>
+                    <Typography variant='h6'>
+                        {waxTherapy.time} min {" "} -  {waxTherapy.price} BGN
+                    </Typography>
+                </div>
+                {/* <Typography dangerouslySetInnerHTML={{ __html: waxTherapy.description }} variant='body2' color='textSecondary' /> */}
+            </CardContent>
+
+            <CardActions disableSpacing className={classes.cardActions} >
+                <div className={classes.cardContent}>
+                    <Typography variant='h6'>
+                        Likes: {likes}
+                    </Typography>
+                </div>
+
+                <IconButton aria-label='Add to Cart' onClick={onClick}>
+
+                    <ThumbUpAlt />
+
+                </IconButton>
+            </CardActions>
+
         </Card>
 
-  )
+    )
 }
 
 export default WaxingItems
