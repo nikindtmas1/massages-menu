f8e4c608da9854e6aa3510db69721db56ccf2958
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-02 16:27:52 +0200

    Update waxingService

--- server/src/services/waxingService.js
+++ server/src/services/waxingService.js
@@ -2,6 +2,10 @@ const Waxing = require('../models/waxingModel');
 
 const getAllWaxing = () => Waxing.find({});
 
+const getOne = (id) => Waxing.findById(id);
+
+
 module.exports = {
     getAllWaxing,
+    getOne,
 }
