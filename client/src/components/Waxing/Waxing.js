551b54d8121dbaee1469632d57eb8eeff10d71a8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-02 16:27:23 +0200

    Update Waxing

--- client/src/components/Waxing/Waxing.js
+++ client/src/components/Waxing/Waxing.js
@@ -3,7 +3,11 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SectionWaxingPage from '../Sections/SectionWaxingPage';
 
+
 const Waxing = () => {
+
+  
+
   return (
     <div id='page3'>
         <div className='bg'>
