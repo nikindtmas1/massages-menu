95e23672cc0e16c0dde55ea175396b12c8fc924d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-03 15:02:33 +0200

    Update WaxingItems

--- client/src/components/WaxingItems/WaxingItems.js
+++ client/src/components/WaxingItems/WaxingItems.js
@@ -1,21 +1,25 @@
 import React, { useState, useContext } from 'react';
+import {useHistory} from 'react-router-dom';
 import { Card, CardMedia, CardContent, Typography, IconButton, CardActions } from '@material-ui/core';
 import useStyles from '../Articles/stylesWaxing';
 import { ThumbUpAlt } from '@material-ui/icons';
 import AuthContext from '../../contexts/AuthCxt';
 
-
+import * as services from '../Services/data';
 
 
 const WaxingItems = ({ waxTherapy }) => {
 
+    const history = useHistory();
+
+    const { name, img, type, time, price, description, likes } = waxTherapy;
     const value = useContext(AuthContext);
 
     const user = value.user.user;
     
     const classes = useStyles();
 
-    const [counter, setCounter] = useState(waxTherapy.likes);
+    const [counter, setCounter] = useState(likes);
     const [count, setCount] = useState(0);
 
     const onClick = () => {
@@ -27,6 +31,13 @@ const WaxingItems = ({ waxTherapy }) => {
                 const currentLikes = counter;
                 setCounter(() => currentLikes + 1);
                 setCount(() => count + 1);
+
+                let newCounter = counter + 1;
+
+                const data = {name, img, type, time, price, description, likes: newCounter}
+                services.editWaxing(waxTherapy._id, data)
+                .then(() => history.push('/waxing'))
+                .catch(error => alert(error.message));
             };
 
         };
@@ -39,14 +50,14 @@ const WaxingItems = ({ waxTherapy }) => {
     return (
 
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={waxTherapy.img} title={waxTherapy.name} />
+            <CardMedia className={classes.media} image={img} title={name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant='h5' gutterBottom>
-                        {waxTherapy.name}
+                        {name}
                     </Typography>
                     <Typography variant='h6'>
-                        {waxTherapy.time} min {" "} -  {waxTherapy.price} BGN
+                        {time} min {" "} -  {price} BGN
                     </Typography>
                 </div>
                 {/* <Typography dangerouslySetInnerHTML={{ __html: waxTherapy.description }} variant='body2' color='textSecondary' /> */}
