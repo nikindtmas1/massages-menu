3e6748a71d5dd8f9c7e84a834cbe4d44e181eb0b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-04 15:39:15 +0200

    Update MassageItems

--- client/src/components/MassageItems/MassageItems.js
+++ client/src/components/MassageItems/MassageItems.js
@@ -4,7 +4,7 @@ import { Typography, IconButton } from '@material-ui/core';
 import { ThumbUpAlt } from '@material-ui/icons';
 
 
-const MassageItems = ({ massages }) => {
+const MassageItems = ({ massage }) => {
 
   const [counter, setCounter] = useState(0);
 
@@ -16,21 +16,22 @@ const MassageItems = ({ massages }) => {
   }
 
   return (
-    <div className="wrapper">
-      {massages.map(x =>
+   <>
+  
+      
         <div className="grid_4 alpha">
 
           <div className="indent-left3">
             <div className="wrapper p2">
-              <figure className="the-images"><img src={x.img} alt="" /></figure>
+              <figure className="the-images"><img src={massage.img} alt="" /></figure>
             </div>
-            <h6 className="prev-indent-bot"><Link className="link" to="#">{x.name}</Link></h6>
+            <h6 className="prev-indent-bot"><Link className="link" to="#">{massage.name}</Link></h6>
             {/* <p className="indent-bot">{x.description}</p> */}
             <p className='indent-bot'>
-              <span style={{ 'padding': "10px", "color": "black" }}>{x.time} min</span>
+              <span style={{ 'padding': "10px", "color": "black" }}>{massage.time} min</span>
               <span>       </span>
 
-              <span style={{ 'padding-left': '120px', 'color': "black" }}>{x.price} BGN</span>
+              <span style={{ 'padding-left': '120px', 'color': "black" }}>{massage.price} BGN</span>
             </p>
             {/* <Link className="button" to="#">Details</Link>  */}
             <p className='indent-bot'>
@@ -56,10 +57,10 @@ const MassageItems = ({ massages }) => {
           <br />
           <br />
         </div>
-      )}
+  
 
-
-    </div>
+</>
+  
   )
 }
 
