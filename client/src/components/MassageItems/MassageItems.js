92096f0a77971eed777adb01e3e7c077fc298392
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-04 14:16:20 +0200

    Update MassagesItems

--- client/src/components/MassageItems/MassageItems.js
+++ client/src/components/MassageItems/MassageItems.js
@@ -1,34 +1,56 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Typography, IconButton } from '@material-ui/core';
+import { ThumbUpAlt } from '@material-ui/icons';
 
-const MassageItems = ({massages}) => {
 
-    
+const MassageItems = ({ massages }) => {
+
+
   return (
     <div className="wrapper">
-     {massages.map(x => 
+      {massages.map(x =>
         <div className="grid_4 alpha">
-            
-        <div className="indent-left3">
-          <div className="wrapper p2">
-            <figure className="the-images"><img src={x.img} alt="" /></figure>
-          </div>
-          <h6 className="prev-indent-bot"><Link className="link" to="#">{x.name}</Link></h6>
-          {/* <p className="indent-bot">{x.description}</p> */}
-          <p className='indent-bot'>
-              <span style={{'padding':"10px","color":"black"}}>{x.time} min</span>
+
+          <div className="indent-left3">
+            <div className="wrapper p2">
+              <figure className="the-images"><img src={x.img} alt="" /></figure>
+            </div>
+            <h6 className="prev-indent-bot"><Link className="link" to="#">{x.name}</Link></h6>
+            {/* <p className="indent-bot">{x.description}</p> */}
+            <p className='indent-bot'>
+              <span style={{ 'padding': "10px", "color": "black" }}>{x.time} min</span>
               <span>       </span>
-         
-              <span style={{'padding-left':'120px', 'color':"black"}}>{x.price} BGN</span>
-              </p>
-          {/* <Link className="button" to="#">Details</Link>  */}
+
+              <span style={{ 'padding-left': '120px', 'color': "black" }}>{x.price} BGN</span>
+            </p>
+            {/* <Link className="button" to="#">Details</Link>  */}
+            <p className='indent-bot'>
+            <span >
+            <Typography>
+                Likes: {0} 
+              <IconButton style={{"padding-left": "140px"}}>
+                <ThumbUpAlt />
+              </IconButton>
+              </Typography>
+            </span>
+              {/* <span>       </span> */}
+
+              {/* <span style={{ 'padding-left': '120px', 'color': "black" }}>
+          
+              </span> */}
+            
+              
+           
+            </p>
+
           </div>
           <br />
           <br />
-      </div>
-        )}
-      
-     
+        </div>
+      )}
+
+
     </div>
   )
 }
