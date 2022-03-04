c48544512fad3d2b17c37c30cf4369abbf50ded8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-04 16:09:46 +0200

    Update MassageService

--- server/src/services/massageService.js
+++ server/src/services/massageService.js
@@ -2,7 +2,12 @@ const Massages = require('../models/massagesModel');
 
 const getAll = () => Massages.find();
 
+const getOneMassage = (id) => Massages.findById(id);
+
+const updateMassage = (id, data) => Massages.findByIdAndUpdate(id, data);
+
 module.exports = {
     getAll,
-    
+    getOneMassage,
+    updateMassage,
 }
