e553325d2217da384aae376c720f858bb26da9b0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-04 15:38:46 +0200

    Update ArticlesPageMassages

--- client/src/components/Articles/ArticlesPageMassages.js
+++ client/src/components/Articles/ArticlesPageMassages.js
@@ -1,25 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import * as services from '../Services/data';
 
 import MassageItems from '../MassageItems/MassageItems';
 const ArticlesPageMassages = () => {
 
-const [massages, setMassages] = useState([]);
+  const [massages, setMassages] = useState([]);
 
-useEffect(() => {
-  services.getAll()
-  .then((result) => setMassages(result))
-},[]);
+  useEffect(() => {
+    services.getAll()
+      .then((result) => setMassages(result))
+  }, []);
 
   return (
     <article className="grid_12">
-    <h3 className="p2">Body Massages</h3>
-    <div className="wrapper p4">
-
-    <MassageItems massages={massages}/>
-    </div>
-  </article>
+      <h3 className="p2">Body Massages</h3>
+      <div className="wrapper p4">
+        <div className='wrapper'>
+          {massages.map((massage) => (
+            <MassageItems massage={massage} />
+          ))}
+        </div>
+      </div>
+    </article>
   )
 }
 
