6b454a5e5d4e68c9584edc24414d1712a95fe653
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 15:18:52 +0200

    Added getOne and editOne logic

--- server/src/controllers/facesController.js
+++ server/src/controllers/facesController.js
@@ -9,4 +9,17 @@ router.get('/', async (req, res) => {
     res.json(faces)
 });
 
+router.get('/:id', async (req, res) => {
+
+    let result = await service.getOneFace(req.params.id);
+    res.json(result);
+ });
+
+ router.put('/:id', async (req, res) => {
+  
+    await service.updateFace(req.params.id, req.body);
+
+    res.json({ok: true});
+});
+
 module.exports = router;
