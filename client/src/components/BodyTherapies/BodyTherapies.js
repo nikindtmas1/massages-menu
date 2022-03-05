7ff057d8510ea8f87de3d45d478e542126f2b12b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 16:33:07 +0200

    Create bodyTherapy Page

---
+++ client/src/components/BodyTherapies/BodyTherapies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const BodyTherapies = () => {
+  return (
+    <div id="page3">
+        <div className="bg">
+            <Header />
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default BodyTherapies
