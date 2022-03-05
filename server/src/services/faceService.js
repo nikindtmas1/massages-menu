34815da9a44db657fea3dbf0588e0caa1f5aafe8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 15:19:43 +0200

    Added getOne ang updateOne in faceService

--- server/src/services/faceService.js
+++ server/src/services/faceService.js
@@ -2,7 +2,12 @@ const Faces = require('../models/faceModel');
 
 const getAllFaces = () => Faces.find();
 
+const getOneFace = (id) => Faces.findById(id);
+
+const updateFace = (id, dataFace) => Faces.findByIdAndUpdate(id, dataFace);
+
 module.exports = {
     getAllFaces,
-    
+    getOneFace,
+    updateFace,
 }
