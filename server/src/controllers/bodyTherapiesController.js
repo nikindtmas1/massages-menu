9b34e07e99a9b378301c30ae24813bf9d0b2395e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 16:25:36 +0200

    Create bodyTherapiesController

---
+++ server/src/controllers/bodyTherapiesController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const services = require('../services/bodyTherapyService');
+
+router.get('/', async (req, res) => {
+
+    const bodyTherapies = await services.getAllBodyTherapies();
+    res.json(bodyTherapies);
+});
+
+router.get('/:id', async (req, res) => {
+
+    const result = await services.getOneBodyTherapy(req.params.id);
+    res.json(result);
+});
+
+router.put('/:id', async (req, res) => {
+
+    await services.updateBodyTherapy(req.params.id, req.body);
+    res.json({ok: true});
+});
+
+module.exports = router;
