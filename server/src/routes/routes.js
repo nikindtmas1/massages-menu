89f9047e41196cc0e2cb14c2ef2de6ea0d3fc88e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 16:26:05 +0200

    Update routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -5,10 +5,12 @@ const userController = require('../controllers/userController');
 const massagesController = require('../controllers/massagesController');
 const facesController = require('../controllers/facesController');
 const waxingController = require('../controllers/waxingController');
+const bodyTherapiesController = require('../controllers/bodyTherapiesController');
 
 router.use('/users', userController);
 router.use('/massages', massagesController);
 router.use('/faces', facesController);
 router.use('/waxings', waxingController);
+router.use('/bodyTherapies', bodyTherapiesController);
 
 module.exports = router;
