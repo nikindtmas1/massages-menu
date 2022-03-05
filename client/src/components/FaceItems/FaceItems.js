24e0b48496474eac8f178cafb8cba9b787d24eb3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 15:34:56 +0200

    Import mui/icons

--- client/src/components/FaceItems/FaceItems.js
+++ client/src/components/FaceItems/FaceItems.js
@@ -1,16 +1,45 @@
-import React, {useContext} from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext, useState, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import AuthCxt from '../../contexts/AuthCxt';
 
 import { Typography, IconButton } from '@material-ui/core';
 import { ThumbUpAlt } from '@material-ui/icons';
 
+import * as services from '../Services/data';
+
 const FaceItems = ({ faceCare }) => {
     
+    const history = useHistory();
     const value = useContext(AuthCxt);
     const user = value.user.user;
 
+    
     const {name, img, type, time, price, description, likes} = faceCare;
+    
+    const [counter, setCounter] = useState(likes);
+    const [count, setCount] = useState(0);
+
+    const onClick = () => {
+
+        if(user){
+
+            if(count < 1){
+
+                let currentLikes = counter;
+                setCounter(() => currentLikes + 1);
+                setCount(() => count + 1);
+
+                let newCounter = counter + 1;
+
+                const data = {name, img, type, time, price, description, likes: newCounter};
+
+                services.editFace(faceCare._id, data)
+                .then(() => history.push('/faceCare'))
+
+            }
+        }
+
+    } 
 
     return (
 
@@ -30,7 +59,7 @@ const FaceItems = ({ faceCare }) => {
                         <Typography>
                             Likes: {likes}
                             <IconButton style={{ "padding-left": "160px" }}>
-                                <ThumbUpAlt />
+                                <ThumbUpAlt onClick={onClick} />
                             </IconButton>
                         </Typography>
                     </span>
