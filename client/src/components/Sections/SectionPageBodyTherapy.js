d6cd4ad5f10340355d0c422cdf1bd0b786af31a6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 16:40:24 +0200

    Create SectionBodyTherapy.js

---
+++ client/src/components/Sections/SectionPageBodyTherapy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ArticlesBodyTherapyPage from '../Articles/ArticlesBodyTherapyPage';
+
+const SectionPageBodyTherapy = () => {
+  return (
+    <section >
+    <div className="main">
+  <div className="container_12">
+    <div className="wrapper">
+        <ArticlesBodyTherapyPage />
+    </div>
+  </div>
+</div>
+   
+</section>
+  )
+}
+
+export default SectionPageBodyTherapy
