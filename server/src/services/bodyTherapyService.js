7bec7a91ce53e04968d12cbd8eeead51f1a62a3b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-05 16:26:21 +0200

    Create bodyTherapyService

---
+++ server/src/services/bodyTherapyService.js
@@ -0,0 +1,14 @@
+const BodyTherapies = require('../models/boduTherapyModel');
+
+const getAllBodyTherapies = () => BodyTherapies.find({});
+
+const getOneBodyTherapy = (id) => BodyTherapies.findById(id);
+
+const updateBodyTherapy = (id, dataTherapy) => BodyTherapies.findByIdAndUpdate(id, dataTherapy);
+
+
+module.exports = {
+    getAllBodyTherapies,
+    getOneBodyTherapy,
+    updateBodyTherapy,
+}
