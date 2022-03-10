9adc15e48583f88558e0da25adfd0767019aa2d6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-10 08:39:15 +0200

    Update ArticlesPageTwo.js

--- client/src/components/Articles/ArticlesPageTwo.js
+++ client/src/components/Articles/ArticlesPageTwo.js
@@ -11,12 +11,12 @@ const ArticlesPageTwo = () => {
               <div className="extra-wrap">
                 <h4>The “I-SPA” Center in Golden Sands </h4>
                 Walk into a dream oasis of over 100 specially curated spa treatments and exotic beauty products, designed to revive and unwind your mind, body and soul. You deserve it.
-The “I-SPA” Center in Golden Sands /Varna, Bulgaria/ offers additional facilities including, indoor pool, steam bath, Turkish bath, infrared sauna, Finnish sauna, hydro-massage room, fitness room and a beauty salon.
-Due to the COVID-19 situation, some of the SPA procedures might be temporary not available. . </div>
+                The “I-SPA” Center in Golden Sands /Varna, Bulgaria/ offers additional facilities including, indoor pool, steam bath, Turkish bath, infrared sauna, Finnish sauna, hydro-massage room, fitness room and a beauty salon.
+                Due to the COVID-19 situation, some of the SPA procedures might be temporary not available. . </div>
             </div>
             To complete your wellness package, we invite you to enjoy our indoor swimming pool or top up your relaxation experience in the Hydro-massage bathtub.
             </article>
-          <article className="grid_3">
+            <article className="grid_3">
             <h3 className="p2">Services List</h3>
             <ul className="list-1">
               <li><Link to="/nailCare">Nailcare</Link></li>
