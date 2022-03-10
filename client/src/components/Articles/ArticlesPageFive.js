24160d87668b73f47d7b8096e0971e35643bcac4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-10 11:29:38 +0200

    Change location

--- client/src/components/Articles/ArticlesPageFive.js
+++ client/src/components/Articles/ArticlesPageFive.js
@@ -68,11 +68,12 @@ const ArticlesPageFive = () => {
           </article>
           <article className="grid_4">
             <h3 className="p2">Postal Address</h3>
-            {/* <figure className="indent-bot">
-              <iframe width="300" height="222" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
-            </figure> */}
+            <figure className="indent-bot">
+              {/* <iframe width="300" height="222" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe> */}
+              <iframe width="300" height="222" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d92947.67027951687!2d28.0371527!3d43.2804473!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4f8d98a418caf%3A0xfd0afef99d2c075d!2sInternational%20Hotel%20Casino%20%26%20Tower%20Suites!5e0!3m2!1sen!2sbg!4v1646904283026!5m2!1sen!2sbg" ></iframe>
+            </figure>
             <dl>
-              <dt className="prev-indent-bot color-3"><strong> Varna, Golden Sands, Street: First</strong></dt>
+              <dt className="prev-indent-bot color-3"><strong>9007 Varna, Golden Sands, Street: First</strong></dt>
               <dd><span>Phone:</span> +359 882 196 877</dd>
               {/* <dd><span>FAX:</span> +1 504 889 9898</dd> */}
               <dd><span>E-mail:</span>spareception@interhc.com</dd>
