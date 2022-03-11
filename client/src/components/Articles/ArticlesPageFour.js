cbfe164c5a54f2e0fe15ac0d6bdc113ba927da24
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-11 08:36:53 +0200

    Added descriptions

--- client/src/components/Articles/ArticlesPageFour.js
+++ client/src/components/Articles/ArticlesPageFour.js
@@ -10,10 +10,10 @@ const ArticlesPageFour = () => {
       <div className="extra-wrap">
         <div className="wrapper">
           <h4 className="fleft">Arzu Mehmed</h4>
-          <ul className="list-services fright indent-top">
-            {/* <li><Link to="#"></Link></li>
-            <li><Link className="item-2" to="#"></Link></li> */}
-          </ul>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
         </div>
         <strong className="text-1 color-3 prev-indent-bot2">Rehabilitation specialist</strong>
         <p>Rehabilitation is a specialty that covers the diagnosis and treatment of physical and functional disorders in the musculoskeletal, nervous or cardiovascular systems.</p>
@@ -25,10 +25,10 @@ const ArticlesPageFour = () => {
       <div className="extra-wrap">
         <div className="wrapper">
           <h4 className="fleft">Zlatina Stancheva</h4>
-          <ul className="list-services fright indent-top">
-            {/* <li><Link to="#"></Link></li>
-            <li><Link className="item-2" to="#"></Link></li> */}
-          </ul>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
         </div>
         <strong className="text-1 color-3 prev-indent-bot2">Rehabilitation specialist</strong>
         <p>Rehabilitation is a specialty that covers the diagnosis and treatment of physical and functional disorders in the musculoskeletal, nervous or cardiovascular systems.</p>
@@ -40,17 +40,76 @@ const ArticlesPageFour = () => {
       <div className="extra-wrap">
         <div className="wrapper">
           <h4 className="fleft">Dani Petrova</h4>
-          <ul className="list-services fright indent-top">
-            {/* <li><Link to="#"></Link></li>
-            <li><Link className="item-2" to="#"></Link></li> */}
-          </ul>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
         </div>
         <strong className="text-1 color-3 prev-indent-bot2">Massage specialist</strong>
         <p>Physical Therapy is a specialty that covers the diagnosis and treatment of physical and functional disorders in the musculoskeletal, nervous or cardiovascular systems.</p>
         {/* <Link className="button" to="#">Details</Link> </div> */}
     </div>
     </div>
-   
+    <div className="wrapper p4">
+      <figure className="img-indent border"><img src="images/page4-img4.png" alt="" /></figure>
+      <div className="extra-wrap">
+        <div className="wrapper">
+          <h4 className="fleft">Maya Ivanova</h4>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
+        </div>
+        <strong className="text-1 color-3 prev-indent-bot2">Receptionist</strong>
+        <p>Receptionist's duties and responsibilities include greeting visitors, helping them navigate through an offices. In addition, they maintain calendars for appointments, sort mail, make copies, and plan travel arrangements.</p>
+        {/* <Link className="button" to="#">Details</Link> </div> */}
+    </div>
+    </div>
+    <div className="wrapper p4">
+      <figure className="img-indent border"><img src="images/page4-img4.png" alt="" /></figure>
+      <div className="extra-wrap">
+        <div className="wrapper">
+          <h4 className="fleft">Rady Dimitrova</h4>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
+        </div>
+        <strong className="text-1 color-3 prev-indent-bot2">Receptionist</strong>
+        <p>Receptionist's duties and responsibilities include greeting visitors, helping them navigate through an offices. In addition, they maintain calendars for appointments, sort mail, make copies, and plan travel arrangements.</p>
+        {/* <Link className="button" to="#">Details</Link> </div> */}
+    </div>
+    </div>
+    <div className="wrapper p4">
+      <figure className="img-indent border"><img src="images/page4-img5.png" alt="" /></figure>
+      <div className="extra-wrap">
+        <div className="wrapper">
+          <h4 className="fleft">Dimitar Eliseev</h4>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
+        </div>
+        <strong className="text-1 color-3 prev-indent-bot2">Lifeguard</strong>
+        <p>A lifeguard is a rescuer who supervises the safety and rescue of swimmers, surfers, and other water sports participants such as in a swimming pool, water park, beach, spa, river and lake. Lifeguards are trained in swimming and CPR/AED first aid, certified in water rescue using a variety of aids and equipment depending on requirements of their particular venue.</p>
+        {/* <Link className="button" to="#">Details</Link> </div> */}
+    </div>
+    </div>
+    <div className="wrapper p4">
+      <figure className="img-indent border"><img src="images/page4-img5.png" alt="" /></figure>
+      <div className="extra-wrap">
+        <div className="wrapper">
+          <h4 className="fleft">Stefan Tonchev</h4>
+          {/* <ul className="list-services fright indent-top">
+            <li><Link to="#"></Link></li>
+            <li><Link className="item-2" to="#"></Link></li>
+          </ul> */}
+        </div>
+        <strong className="text-1 color-3 prev-indent-bot2">Lifeguard</strong>
+        <p>A lifeguard is a rescuer who supervises the safety and rescue of swimmers, surfers, and other water sports participants such as in a swimming pool, water park, beach, spa, river and lake. Lifeguards are trained in swimming and CPR/AED first aid, certified in water rescue using a variety of aids and equipment depending on requirements of their particular venue.</p>
+        {/* <Link className="button" to="#">Details</Link> </div> */}
+    </div>
+    </div>
   </article>
   )
 }
