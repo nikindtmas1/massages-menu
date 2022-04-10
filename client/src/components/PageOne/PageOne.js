b855c7d11511e348a5d50e473419490a609136b6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-10 16:59:16 +0300

    Change import pages

--- client/src/components/PageOne/PageOne.js
+++ client/src/components/PageOne/PageOne.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Header from '../Header/Header';
+//import Header from '../Header/Header';
 import SectionPageOne from '../Sections/SectionPageOne';
-import Footer from '../Footer/Footer';
-import AsidePageOne from '../Asides/AsidePageOne';
+//import Footer from '../Footer/Footer';
+//import AsidePageOne from '../Asides/AsidePageOne';
 
 const PageOne = () => {
   return (
     <>
-     <div id="page1">
+            <SectionPageOne />
+     {/* <div id="page1">
         <div className="bg">
             <Header />
             <SectionPageOne />
         </div>
         <AsidePageOne />
         <Footer />
-    </div>
+    </div> */}
     </>
   )
 }
