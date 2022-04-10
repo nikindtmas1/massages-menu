f4671709493fa172a46ee517020ced3ce8b0e7ec
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-10 17:42:25 +0300

    Added search input

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,12 +1,59 @@
 //import { Tab } from "@mui/material";
+//import { Search } from "@material-ui/icons";
+import { styled, alpha } from '@mui/material/styles';
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 //import ToggleButton from '@mui/material/ToggleButton';
 import AuthCxt from '../../contexts/AuthCxt';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+
+
 //import styles from './Navigation.module.css';
 //import ColorToggleButton from "./NavTab";
 //import TabsWrappedLabel from "./NavTabSec";
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const Navigation = () => {
 
@@ -57,7 +104,7 @@ const Navigation = () => {
   let isAuth = value.user.isAuthenticated;
 
   let guestNav = (
-   
+   <>
     <ul className="menu">
     <li >
       <Link className={isActive ? 'active' : ''}
@@ -85,7 +132,20 @@ const Navigation = () => {
     {/* <li >
       <Link to="/logout">Logout</Link>
     </li> */}
-  </ul>
+      <li style={{'padding':'30px'}}>
+      <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+      </li>
+       
+          </ul>
+  </>
   );
 
   let userNav = (
@@ -117,6 +177,17 @@ const Navigation = () => {
     <li >
       <Link to="/logout">Logout</Link>
     </li>
+    {/* <li style={{'padding':'30px'}}>
+      <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+      </li> */}
   </ul>
     <span style={{"margin": "20px", "padding": "20px", "display": "flex"}}>Welcome: {user}</span>
     </>
