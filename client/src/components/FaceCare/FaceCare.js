718ec0d9ec56fac456d750b00e1c900916dacb6c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-11 11:02:31 +0300

    Change page face care

--- client/src/components/FaceCare/FaceCare.js
+++ client/src/components/FaceCare/FaceCare.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Header from '../Header/Header';
+//import Header from '../Header/Header';
 import SectionFacePage from '../Sections/SectionFacePage';
-import Footer from '../Footer/Footer';
+//import Footer from '../Footer/Footer';
 
 const FaceCare = () => {
   return (
-    <div id='page3'>
-        <div className='bg'>
-            <Header />
-            <SectionFacePage />
-        </div>
-        <Footer />
-    </div>
+
+    <SectionFacePage />
+
+    // <div id='page3'>
+    //     <div className='bg'>
+    //         <Header />
+    //         <SectionFacePage />
+    //     </div>
+    //     <Footer />
+    // </div>
   )
 }
 
