e26d3e09ed73943eba49eafd11c380b826b8bab7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-11 11:02:03 +0300

    Change page body massages

--- client/src/components/BodyMassages/BodyMassages.js
+++ client/src/components/BodyMassages/BodyMassages.js
@@ -1,17 +1,20 @@
 import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+//import Header from "../Header/Header";
+//import Footer from "../Footer/Footer";
 import SectionPageMassages from "../Sections/SectionPageMassages";
 
 const BodyMassages = () => {
   return (
-    <div id="page3">
-      <div className="bg">
-          <Header />
-          <SectionPageMassages />
-          </div>
-        <Footer />
-    </div>
+
+    <SectionPageMassages />
+
+    // <div id="page3">
+    //   <div className="bg">
+    //       <Header />
+    //       <SectionPageMassages />
+    //       </div>
+    //     <Footer />
+    // </div>
   );
 };
 
